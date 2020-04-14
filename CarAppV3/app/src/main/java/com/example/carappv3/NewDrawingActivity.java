57b3f9b3e43c3993ab67ff63ebd5eaadb95a72c6
Siqi Chai <sqchai@ucdavis.edu>
2020-04-14 02:11:21 -0700

    implement back arrow in action bar

--- CarAppV3/app/src/main/java/com/example/carappv3/NewDrawingActivity.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/NewDrawingActivity.java
@@ -1,8 +1,10 @@
 package com.example.carappv3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class NewDrawingActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class NewDrawingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_drawing);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
