91a1065c05128e0039d4c7a49735e81a98e00c73
Siqi Chai <sqchai@ucdavis.edu>
2020-04-13 23:03:51 -0700

    setup details in design detail activity

--- CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DesignDetailsActivity extends AppCompatActivity {
 
@@ -11,12 +12,22 @@ public class DesignDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_design_details);
+
+        getIntentExtra();
     }
 
     private void getIntentExtra() {
         if(getIntent().hasExtra("position") && getIntent().hasExtra("name")) {
             int position = getIntent().getIntExtra("position", 0);
             String name = getIntent().getStringExtra("name");
+
+            setDetailsView(position, name);
         }
+    }
+
+    private void setDetailsView(int position, String name) {
+        String details = "Id #" + position + ": " + name;
+        TextView textView = (TextView)findViewById(R.id.design_detail_text);
+        textView.setText(details);
     }
 }
