e1e8412ff75f452c91914efa9bc6c85e8c32a4e3
Siqi Chai <sqchai@ucdavis.edu>
2020-04-13 21:50:03 -0700

    Recycler Text View test passed

--- CarAppV3/app/src/main/java/com/example/carappv3/DrawingListAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.example.carappv3;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-public class DrawingListAdapter extends RecyclerView. {
-}
--- CarAppV3/app/src/main/java/com/example/carappv3/DrawingListFragment.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/DrawingListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -36,11 +37,13 @@ public class DrawingListFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_drawing_list, container, false);
 
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycle_view_1);
-        RecyclerAdapter
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycle_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(getContext(), items);
+        recyclerView.setAdapter(recyclerAdapter);
 
-
-        recyclerView.setAdapter(recyclerViewAdapter);
         return view;
     }
 }
