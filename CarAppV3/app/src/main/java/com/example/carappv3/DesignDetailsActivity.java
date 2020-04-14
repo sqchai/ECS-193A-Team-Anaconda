0ff3938a7a73e4d6d247d93614634f099dd5359c
Siqi Chai <sqchai@ucdavis.edu>
2020-04-14 02:12:06 -0700

    Update DesignDetailsActivity.java

--- CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
@@ -1,9 +1,11 @@
 package com.example.carappv3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DesignDetailsActivity extends AppCompatActivity {
@@ -12,8 +14,20 @@ public class DesignDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_design_details);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
         getIntentExtra();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void getIntentExtra() {
