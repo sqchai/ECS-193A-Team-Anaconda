26ad69625b1f8d4a0b6f0f829d923abaf15b5ea0
Siqi Chai <sqchai@ucdavis.edu>
2020-04-13 21:18:32 -0700

    recycler view holder

--- CarAppV3/app/src/main/java/com/example/carappv3/RecyclerAdapter.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/RecyclerAdapter.java
@@ -1,8 +1,25 @@
 package com.example.carappv3;
 
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
 public class RecyclerAdapter {
     private ArrayList<String> names = new ArrayList<>();
-    
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        RelativeLayout relativeLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.recycler_text);
+            relativeLayout = itemView.findViewById(R.id.recycler_layout);
+        }
+    }
 }
