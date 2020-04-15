6b4d8f792f369dd47fedbd20a1316d7a32b7f998
Siqi Chai <sqchai@ucdavis.edu>
2020-04-15 15:01:19 -0700

    Update DrawingView.java

--- CarAppV3/app/src/main/java/com/example/carappv3/DrawingView.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/DrawingView.java
@@ -24,13 +24,14 @@ public class DrawingView extends View {
     public static final int DRAWING_COLOR = R.color.colorRoseRed;
     public static final int CANVAS_COLOR = R.color.colorWhite;
     public static final int TOUCH_TOLERANCE = 4;
+    public static final int STROKE_WIDTH = 1;
 
     private float currX;
     private float currY;
     private Canvas canvas;
     private Paint paint;
-    private ArrayList<Path> paths = new  ArrayList<>();
-    private ArrayList<Path> redo = new  ArrayList<>();
+    private ArrayList<DrawingPath> paths = new  ArrayList<>();
+    private ArrayList<DrawingPath> redo = new  ArrayList<>();
 
     public DrawingView(Context context) {
         super(context);
@@ -58,128 +59,22 @@ public class DrawingView extends View {
         paint = new Paint();
         paint.setAntiAlias(true);
         paint.setColor(getResources().getColor(DRAWING_COLOR));
+        paint.setStrokeWidth(STROKE_WIDTH);
+        paint.setMaskFilter(null);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStrokeCap(Paint.Cap.ROUND);
-
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
-    }
-
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mExampleDimension);
-        mTextPaint.setColor(mExampleColor);
-        mTextWidth = mTextPaint.measureText(mExampleString);
-
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
-
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        // Draw the text.
-        canvas.drawText(mExampleString,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);
-
-        // Draw the example drawable on top of the text.
-        if (mExampleDrawable != null) {
-            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-                    paddingLeft + contentWidth, paddingTop + contentHeight);
-            mExampleDrawable.draw(canvas);
+        canvas.save();
+        canvas.drawColor(getResources().getColor(CANVAS_COLOR));
+        for(DrawingPath drawingPath : paths) {
+            canvas.drawPath(drawingPath.path, paint);
         }
+        //TODO: update json file
     }
 
-    /**
-     * Gets the example string attribute value.
-     *
-     * @return The example string attribute value.
-     */
-    public String getExampleString() {
-        return mExampleString;
-    }
-
-    /**
-     * Sets the view's example string attribute value. In the example view, this string
-     * is the text to draw.
-     *
-     * @param exampleString The example string attribute value to use.
-     */
-    public void setExampleString(String exampleString) {
-        mExampleString = exampleString;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example color attribute value.
-     *
-     * @return The example color attribute value.
-     */
-    public int getExampleColor() {
-        return mExampleColor;
-    }
-
-    /**
-     * Sets the view's example color attribute value. In the example view, this color
-     * is the font color.
-     *
-     * @param exampleColor The example color attribute value to use.
-     */
-    public void setExampleColor(int exampleColor) {
-        mExampleColor = exampleColor;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example dimension attribute value.
-     *
-     * @return The example dimension attribute value.
-     */
-    public float getExampleDimension() {
-        return mExampleDimension;
-    }
-
-    /**
-     * Sets the view's example dimension attribute value. In the example view, this dimension
-     * is the font size.
-     *
-     * @param exampleDimension The example dimension attribute value to use.
-     */
-    public void setExampleDimension(float exampleDimension) {
-        mExampleDimension = exampleDimension;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example drawable attribute value.
-     *
-     * @return The example drawable attribute value.
-     */
-    public Drawable getExampleDrawable() {
-        return mExampleDrawable;
-    }
-
-    /**
-     * Sets the view's example drawable attribute value. In the example view, this drawable is
-     * drawn above the text.
-     *
-     * @param exampleDrawable The example drawable attribute value to use.
-     */
-    public void setExampleDrawable(Drawable exampleDrawable) {
-        mExampleDrawable = exampleDrawable;
-    }
 }
