4cf0df44aaf9e7b41cc8628d06127c06dc32db4c
Siqi Chai <sqchai@ucdavis.edu>
2020-04-20 23:16:10 -0700

    Wireless connection between phone and NodeMCU. This is an .ino code that runs on the NodeMCU

---
+++ NodeMCU/server/server/server.ino
@@ -0,0 +1,36 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server;
+uint8_t pin_led = 16;
+
+char* ssid = "X Air";
+char* password = "Jiang991022";
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin_led, OUTPUT);
+  WiFi.begin(ssid, password);
+  Serial.begin(9600);
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("");
+  Serial.print("IP Address: ");
+  Serial.print(WiFi.localIP());
+
+  server.on("/", [](){server.send(200, "text/plain", "Hello World!");});
+  server.on("/toggle", toggleLED);
+  server.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  server.handleClient();
+}
+
+void toggleLED() {
+  digitalWrite(pin_led, !digitalRead(pin_led));
+  server.send(204, "");
+}
