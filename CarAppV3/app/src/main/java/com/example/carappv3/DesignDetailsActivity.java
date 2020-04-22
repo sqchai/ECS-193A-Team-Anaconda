f488b73ddc7b6517539e3ea95731946c048bb68a
Siqi Chai <sqchai@ucdavis.edu>
2020-04-22 00:13:03 -0700

    Update DesignDetailsActivity.java

--- CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class DesignDetailsActivity extends AppCompatActivity {
 
@@ -21,15 +29,25 @@ public class DesignDetailsActivity extends AppCompatActivity {
         getIntentExtra();
 
         //test WiFi Connection
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
-
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url ="http://10.0.0.86";
+        final StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Response: ", response);
+            }
+        }, new Response.ErrorListener () {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("No Response: ", "");
+            }
+        });
 
         Button wifiButton = findViewById(R.id.wifi_button);
         wifiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                requestQueue.add(stringRequest);
             }
         });
     }
