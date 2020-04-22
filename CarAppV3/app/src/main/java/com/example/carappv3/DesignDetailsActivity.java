b59eefd46db977c52fa01d40f550617e6cc97842
Siqi Chai <sqchai@ucdavis.edu>
2020-04-21 23:55:36 -0700

    WiFi send button

--- CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
@@ -17,6 +17,9 @@ public class DesignDetailsActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getIntentExtra();
+
+        //test WiFi Connection
+
     }
 
     @Override
