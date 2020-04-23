c12b05268d2c901466396bc19413b8044dbae614
pathac <xuczhou@ucdavis.edu>
2020-04-22 19:16:40 -0700

    broken raycast experiment but success distance recognization

--- April8/April8/ContentView.swift
+++ April8/April8/ContentView.swift
@@ -56,14 +56,17 @@ struct HomeView: View {
 struct ARViewContainer: UIViewRepresentable {
 
     func makeUIView(context: Context) -> ARView {
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
         
         let arView = ARView(frame: .zero)
         ARDelegate.set(arView: arView)
         arView.session.delegate = ARDelegate
         let config = ARWorldTrackingConfiguration()
-        config.planeDetection = [.horizontal, .vertical]
+        //config.planeDetection = [.horizontal, .vertical]
         config.isLightEstimationEnabled = true
-        
+        config.detectionImages = referenceImages
         // Load the "Box" scene from the "Experience" Reality File
         //let boxAnchor = try! Experience.loadBox()
         
@@ -71,6 +74,7 @@ struct ARViewContainer: UIViewRepresentable {
         //arView.scene.anchors.append(boxAnchor)
         
         arView.debugOptions = [ARView.DebugOptions.showAnchorGeometry]
+        arView.setupGestures()
         arView.session.run(config)
         return arView
         
@@ -91,10 +95,107 @@ final class SessionDelegate: NSObject, ARSessionDelegate{
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         print("added anchor(s)")
         print(anchors[anchors.count-1].transform)
+        
+        guard let imageAnchor = anchors[anchors.count-1] as? ARImageAnchor else { return }
+        //let referenceImage = imageAnchor.referenceImage
+        let mtransform = imageAnchor.transform
+        let box = CustomBox(color: .yellow, position: SIMD3(mtransform.columns.3.x, mtransform.columns.3.y, mtransform.columns.3.z))
+        arView.scene.anchors.append(box)
+        /*let plane = SCNPlane(width: referenceImage.physicalSize.width,
+                             height: referenceImage.physicalSize.height)
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.opacity = 0.25
+        planeNode.eulerAngles.x = -.pi / 2
+        arView.scene.addAnchor(<#T##anchor: HasAnchoring##HasAnchoring#>)*/
     }
 }
 
+extension ARView{
+    func setupGestures() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        self.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        
+        guard let touchInView = sender?.location(in: self) else {
+            return
+        }
+        
+        rayCastingMethod(point: touchInView)
+        
+        //to find whether an entity exists at the point of contact
+        let entities = self.entities(at: touchInView)
+    }
+    
+    func rayCastingMethod(point: CGPoint) {
+        
+        
+        guard let coordinator = self.session.delegate as? SessionDelegate else{ print("GOOD NIGHT"); return }
+        
+        guard let raycastQuery = self.makeRaycastQuery(from: point,
+                                                       allowing: .existingPlaneInfinite,
+                                                       alignment: .horizontal) else {
+                                                        
+                                                        print("failed first")
+                                                        return
+        }
+        
+        guard let result = self.session.raycast(raycastQuery).first else {
+            print("failed")
+            return
+        }
+        
+        let transformation = Transform(matrix: result.worldTransform)
+        let box = CustomBox(color: .gray)
+        self.installGestures(.all, for: box)
+        box.generateCollisionShapes(recursive: true)
+        
+        let mesh = MeshResource.generateText(
+            "abcd",
+            extrusionDepth: 0.1,
+            font: .systemFont(ofSize: 2),
+            containerFrame: .zero,
+            alignment: .left,
+            lineBreakMode: .byTruncatingTail)
+        
+        let material = SimpleMaterial(color: .red, isMetallic: false)
+        let entity = ModelEntity(mesh: mesh, materials: [material])
+        entity.scale = SIMD3<Float>(0.03, 0.03, 0.1)
+        
+        box.addChild(entity)
+        box.transform = transformation
+        
+        entity.setPosition(SIMD3<Float>(0, 0.05, 0), relativeTo: box)
+        
+        let raycastAnchor = AnchorEntity(raycastResult: result)
+        raycastAnchor.addChild(box)
+        self.scene.addAnchor(raycastAnchor)
+    }
+}
 
+class CustomBox: Entity, HasModel, HasAnchoring, HasCollision {
+    
+    required init(color: UIColor) {
+        super.init()
+        self.components[ModelComponent] = ModelComponent(
+            mesh: .generateBox(size: 0.1),
+            materials: [SimpleMaterial(
+                color: color,
+                isMetallic: false)
+            ]
+        )
+    }
+    
+    convenience init(color: UIColor, position: SIMD3<Float>) {
+        self.init(color: color)
+        self.position = position
+    }
+    
+    required init() {
+        fatalError("init() has not been implemented")
+    }
+}
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
