61a45479255483c682149c1a3904a5efdddcb3b3
Siqi Chai <sqchai@ucdavis.edu>
2020-04-23 00:13:22 -0700

    Update DesignDetailsActivity.java

--- CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/DesignDetailsActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -49,6 +50,24 @@ public class DesignDetailsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 requestQueue.add(stringRequest);
+            }
+        });
+
+        final SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
