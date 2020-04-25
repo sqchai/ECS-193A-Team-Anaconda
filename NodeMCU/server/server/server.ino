8065aeb63f61590d8891634dae2b17a621a997be
Siqi Chai <sqchai@ucdavis.edu>
2020-04-25 00:38:04 -0700

    Update server.ino

--- NodeMCU/server/server/server.ino
+++ NodeMCU/server/server/server.ino
@@ -1,13 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <Servo.h>
 
 ESP8266WebServer server;
+
+//LED
 uint8_t pin_led = 16;
 
 //motors
 uint8_t m1 = D1;
 uint8_t m1_dir = D3;
 int m1_speed = 0;
+
+//servo
+uint8_t s1_port = 7;
+Servo s1;
 
 char* ssid = "X Air";
 char* password = "Jiang991022";
@@ -21,6 +28,9 @@ void setup() {
   pinMode(m1_dir, OUTPUT);
   analogWrite(m1, m1_speed);
   digitalWrite(m1_dir, LOW);
+
+  //setup servo
+  s1.attach(7);
   
   WiFi.begin(ssid, password);
   Serial.begin(9600);
@@ -37,6 +47,10 @@ void setup() {
 
   //motor test
   server.on("/m1", motor1);
+
+  //servo test
+  server.on("/servo1", servo1);
+  
   server.begin();
 }
 
@@ -53,4 +67,20 @@ void toggleLED() {
 void motor1() {
   m1_speed = server.arg("m1speed").toInt();
   analogWrite(m1, m1_speed);
+  digitalWrite(pin_led, !digitalRead(pin_led));
+  server.send(204, "received");
+}
+
+void servo1() {
+  //angle = server.arg("servoAngle").toInt();
+  int angle = s1.read();
+  if(angle < 10) {
+    angle = 10;
+  } else if(angle > 170) {
+    angle = 170;
+  }
+
+  s1.write(170-angle);
+  digitalWrite(pin_led, !digitalRead(pin_led));
+  server.send(204, "received");
 }
