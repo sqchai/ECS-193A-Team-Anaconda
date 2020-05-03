e80d467027ba3982a39f4cfbf941859493460511
Siqi Chai <sqchai@ucdavis.edu>
2020-05-03 16:42:03 -0700

    MLkit

--- MLKitTest/app/src/main/java/com/example/mlkittest/MainActivity.java
+++ MLKitTest/app/src/main/java/com/example/mlkittest/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetector;
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,4 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    FirebaseVisionObjectDetectorOptions options =
+            new FirebaseVisionObjectDetectorOptions.Builder()
+                    .setDetectorMode(FirebaseVisionObjectDetectorOptions.STREAM_MODE)
+                    .build();
+    FirebaseVisionObjectDetector objectDetector =
+            FirebaseVision.getInstance().getOnDeviceObjectDetector(options);
 }
