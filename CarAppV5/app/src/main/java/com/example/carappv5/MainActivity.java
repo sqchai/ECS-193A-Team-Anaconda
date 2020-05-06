cc45e5e3e200a5c6302e8b6d185e562b92f79e50
Siqi Chai <sqchai@ucdavis.edu>
2020-05-05 18:08:30 -0700

    Main

---
+++ CarAppV5/app/src/main/java/com/example/carappv5/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.carappv5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTrackerActivity();
+            }
+        });
+    }
+
+    private void startTrackerActivity() {
+        Intent intent = new Intent(this, Tracker.class);
+        startActivity(intent);
+    }
+}
