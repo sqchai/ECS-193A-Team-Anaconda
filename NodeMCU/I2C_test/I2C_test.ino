d8a1a63eed1f058dc02ca1cc510df603c1bdf84e
Siqi Chai <sqchai@ucdavis.edu>
2020-05-07 02:50:06 -0700

    Update I2C_test.ino

--- NodeMCU/I2C_test/I2C_test.ino
+++ NodeMCU/I2C_test/I2C_test.ino
@@ -13,14 +13,14 @@ float Acc[2];
 float Gy[3];
 float Angle[3];
 
-String valores;
+String values;
 
-long tiempo_prev;
+long time_prevs;
 float dt;
 
 void setup()
 {
-Wire.begin(4,5); // D2(GPIO4)=SDA / D1(GPIO5)=SCL
+Wire.begin(12,14); // D2(GPIO4)=SDA / D1(GPIO5)=SCL
 Wire.beginTransmission(MPU);
 Wire.write(0x6B);
 Wire.write(0);
@@ -38,10 +38,10 @@ void loop()
    AcX=Wire.read()<<8|Wire.read();
    AcY=Wire.read()<<8|Wire.read();
    AcZ=Wire.read()<<8|Wire.read();
- /*
+ 
    Acc[1] = atan(-1*(AcX/A_R)/sqrt(pow((AcY/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
    Acc[0] = atan((AcY/A_R)/sqrt(pow((AcX/A_R),2) + pow((AcZ/A_R),2)))*RAD_TO_DEG;
- */
+ 
   
    Wire.beginTransmission(MPU);
    Wire.write(0x43);
@@ -55,24 +55,20 @@ void loop()
    Gy[1] = GyY/G_R;
    Gy[2] = GyZ/G_R;
 
-/*
-   dt = (millis() - tiempo_prev) / 1000.0;
-   tiempo_prev = millis();
+
+   dt = (millis() - time_prevs) / 1000.0;
+   time_prevs = millis();
  
-   //Aplicar el Filtro Complementario
+   
    Angle[0] = 0.98 *(Angle[0]+Gy[0]*dt) + 0.02*Acc[0];
    Angle[1] = 0.98 *(Angle[1]+Gy[1]*dt) + 0.02*Acc[1];
 
-   //Integración respecto del tiempo paras calcular el YAW
    Angle[2] = Angle[2]+Gy[2]*dt;
  
-   //Mostrar los valores por consola
-   valores = "90, " +String(Angle[0]) + "," + String(Angle[1]) + "," + String(Angle[2]) + ", -90";
- */  
-   Serial.print(Gy[0]);
-   Serial.print(Gy[1]);
-   Serial.print(Gy[2]);
-   Serial.println();
+   values = String(Angle[0]) + "," + String(Angle[1]) + "," + String(Angle[2]);
+  
    
-   delay(10);
+   Serial.println(values);
+   
+   delay(100);
 }
