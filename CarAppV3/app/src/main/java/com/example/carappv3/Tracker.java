694d8858187d670fe7a357f3a4c6db8a1f67bf1f
Siqi Chai <sqchai@ucdavis.edu>
2020-05-20 03:32:17 -0700

    Update Tracker.java

--- CarAppV3/app/src/main/java/com/example/carappv3/Tracker.java
+++ CarAppV3/app/src/main/java/com/example/carappv3/Tracker.java
@@ -45,10 +45,188 @@ import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions
 import java.util.List;
 
 public class Tracker extends AppCompatActivity {
+    private int REQUEST_CODE_PERMISSIONS = 101;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    TextureView textureView;
+
+    TextView centerXView;
+    TextView centerYView;
+
+    //ML Kit Object Detector
+    FirebaseVisionObjectDetector firebaseVisionObjectDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
+
+        textureView = findViewById(R.id.view_finder);
+
+        centerXView = findViewById(R.id.centerx_view);
+        centerXView.setText("0");
+        centerYView = findViewById(R.id.centery_view);
+        centerYView.setText("0");
+
+        // path following algorithm encapsulated in guider
+
+        //init ML Kit Vision Detector
+        FirebaseVisionObjectDetectorOptions options =
+                new FirebaseVisionObjectDetectorOptions.Builder()
+                        .setDetectorMode(FirebaseVisionObjectDetectorOptions.STREAM_MODE)
+                        .build();
+        firebaseVisionObjectDetector = FirebaseVision.getInstance().getOnDeviceObjectDetector(options);
+        //Check Permission and start camera
+        if(allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
     }
+
+    private boolean allPermissionsGranted(){
+
+        for(String permission : REQUIRED_PERMISSIONS){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == REQUEST_CODE_PERMISSIONS){
+            if(allPermissionsGranted()){
+                startCamera();
+            } else{
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+
+    private void startCamera() {
+        CameraX.unbindAll();
+
+        Rational aspectRatio = new Rational (textureView.getWidth(), textureView.getHeight());
+        Size screen = new Size(textureView.getWidth(), textureView.getHeight()); //size of the screen
+        PreviewConfig pConfig = new PreviewConfig.Builder()
+                .setTargetAspectRatio(aspectRatio)
+                .setTargetResolution(screen)
+                .build();
+        Preview preview = new Preview(pConfig);
+
+        preview.setOnPreviewOutputUpdateListener(
+                new Preview.OnPreviewOutputUpdateListener() {
+                    //to update the surface texture we  have to destroy it first then re-add it
+                    @Override
+                    public void onUpdated(Preview.PreviewOutput output){
+                        ViewGroup parent = (ViewGroup) textureView.getParent();
+                        parent.removeView(textureView);
+                        parent.addView(textureView, 0);
+
+                        textureView.setSurfaceTexture(output.getSurfaceTexture());
+                        updateTransform();
+                    }
+                });
+
+
+        ImageAnalysisConfig aConfig = new ImageAnalysisConfig.Builder()
+                .setTargetAspectRatio(aspectRatio)
+                .setTargetResolution(new Size(480, 640))
+                .build();
+        ImageAnalysis analysis = new ImageAnalysis(aConfig);
+
+        analysis.setAnalyzer(new ImageAnalysis.Analyzer() {
+            private int degreesToFirebaseRotation(int degrees) {
+                switch (degrees) {
+                    case 0:
+                        return FirebaseVisionImageMetadata.ROTATION_0;
+                    case 90:
+                        return FirebaseVisionImageMetadata.ROTATION_90;
+                    case 180:
+                        return FirebaseVisionImageMetadata.ROTATION_180;
+                    case 270:
+                        return FirebaseVisionImageMetadata.ROTATION_270;
+                    default:
+                        throw new IllegalArgumentException(
+                                "Rotation must be 0, 90, 180, or 270.");
+                }
+            }
+
+            @Override
+            public void analyze(ImageProxy image, int rotationDegrees) {
+                if (image == null || image.getImage() == null) {
+                    return;
+                }
+                Image mediaImage = image.getImage();
+                int rotation = degreesToFirebaseRotation(rotationDegrees);
+                FirebaseVisionImage firebaseVisionImage =
+                        FirebaseVisionImage.fromMediaImage(mediaImage, rotation);
+
+                firebaseVisionObjectDetector.processImage(firebaseVisionImage)
+                        .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionObject>>() {
+                            @Override
+                            public void onSuccess(List<FirebaseVisionObject> firebaseVisionObjects) {
+                                //Log.d("Object Detector", "Success");
+                                for (FirebaseVisionObject obj : firebaseVisionObjects) {
+                                    Integer id = obj.getTrackingId();
+                                    Rect bounds = obj.getBoundingBox();
+
+                                    //Log.d("OBJ_DETECTED", (id+": "+bounds.centerX() + " | "+bounds.centerY()));
+                                    centerXView.setText(Integer.toString(bounds.centerX()));
+                                    centerYView.setText(Integer.toString(bounds.centerY()));
+
+                                    //guider.updateCarPos((int)bounds.centerX(), (int)bounds.centerY());
+                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d("Object Detector", e.toString());
+                            }
+                        });
+            }
+        });
+
+
+        CameraX.bindToLifecycle((LifecycleOwner)this, preview, analysis);
+    }
+
+    private void updateTransform(){
+        Matrix mx = new Matrix();
+        float w = textureView.getMeasuredWidth();
+        float h = textureView.getMeasuredHeight();
+
+        float cX = w / 2f;
+        float cY = h / 2f;
+
+        int rotationDgr;
+        int rotation = (int)textureView.getRotation();
+
+        switch(rotation){
+            case Surface.ROTATION_0:
+                rotationDgr = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotationDgr = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotationDgr = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotationDgr = 270;
+                break;
+            default:
+                return;
+        }
+
+        mx.postRotate((float)rotationDgr, cX, cY);
+        textureView.setTransform(mx);
+    }
+
 }
