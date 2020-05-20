2b4118bb6f1dd89c7287ec3873c3e4989fb5b902
Siqi Chai <sqchai@ucdavis.edu>
2020-05-20 03:28:44 -0700

    Create Tracker.java

---
+++ CarAppV3/app/src/main/java/com/example/carappv3/Tracker.java
@@ -0,0 +1,14 @@
+package com.example.carappv3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
